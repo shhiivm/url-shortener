807c32587ecaa4028e93eecf43bf179f2f6dd087
shhiivm <shhiivm@pm.me>
2025-09-25 22:59:25 +0530

    connection established

---
+++ controllers/url.js
@@ -0,0 +1,20 @@
+const { nanoid } = require("nanoid");
+const URL = require("../models/url");
+async function handleGenerateNewShortUrl(req, res) {
+  const body = req.body;
+  if (!body.url)
+    return res.status(400).json({
+      error: "url is required",
+    });
+  const shortId = nanoid(8);
+  await URL.create({
+    shortId: shortId,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
+  return res.json({ id: shortId });
+}
+
+module.exports = {
+  handleGenerateNewShortUrl,
+};
--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
 
 const app = express();
 const PORT = 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello from express");
+connectToMongoDB("mongodb://localhost:27017/short-url").then(() => {
+  console.log("Mongo db connected");
 });
+
+app.use(express.json());
+
+app.use("/url", urlRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is live on PORT http://localhost:${PORT}`);
